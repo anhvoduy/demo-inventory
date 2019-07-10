27d8e0c4d24d46d7023ef60cc1fccfaba4e6c6b1
devinsg <duyanh2005@gmail.com>
2019-07-10 16:46:42 +0700

    clean up & optimize code

--- app.js
+++ app.js
@@ -1,33 +1,14 @@
 const express = require('express');
-const http = require('http');
-const path = require("path");
-
+const path = require('path');
 const app = express();
 app.set('port', process.env.PORT || 9090);
 
-const webpath = path.join(__dirname, 'build');
-app.use('/', express.static(webpath, { index: 'index.html' }));
-app.get('/', function (req, res) {
-    res.sendFile(path.join(webpath, 'index.html'));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-http.createServer(app).listen(app.get('port'), function () {
-    console.log('React Hook is running on port:' + app.get('port'));
+app.listen(app.get('port'), function () {
+  console.log('React Hook is running on port:' + app.get('port'));
 });
-
-
-// const express = require('express');
-// const bodyParser = require('body-parser')
-// const path = require('path');
-// const app = express();
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// app.get('/ping', function (req, res) {
-//  return res.send('pong');
-// });
-
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
-// app.listen(process.env.PORT || 8080);
