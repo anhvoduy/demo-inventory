4e9eaf1079d2a6ca1cd0e04fea69db40c1628677
Anh <voduyanh1984@gmail.com>
2020-06-15 15:12:11 +0700

    - make coins2

--- pushcoins/build/precache-manifest.662046a88d334423fd326ca46e9eb781.js
+++ pushcoins/build/precache-manifest.662046a88d334423fd326ca46e9eb781.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3893cff3d149c9797d1e83983963edc5",
+    "revision": "373cffc031fd17899e0d760c690aff95",
     "url": "/index.html"
   },
   {
-    "revision": "6931110d7fe726689ac9",
-    "url": "/static/css/main.bfd9321d.chunk.css"
+    "revision": "c79e0206723267ca8bd6",
+    "url": "/static/css/main.523f09c5.chunk.css"
   },
   {
     "revision": "38feb2d52c736de91002",
@@ -16,15 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.5323087f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6931110d7fe726689ac9",
-    "url": "/static/js/main.ff9fe7b5.chunk.js"
+    "revision": "c79e0206723267ca8bd6",
+    "url": "/static/js/main.baafee42.chunk.js"
   },
   {
     "revision": "f2473ced86f1d968a1f0",
     "url": "/static/js/runtime-main.1b5bcfd1.js"
   },
   {
-    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/static/media/logo.5d5d9eef.svg"
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
   }
 ]);
