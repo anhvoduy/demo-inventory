2241415ac108bdc5fb5e2f086f86d20010196bea
Anh <voduyanh1984@gmail.com>
2020-06-15 11:04:22 +0700

    - merge admin layout to structure

--- admin/src/components/app/container.js
+++ admin/src/components/app/container.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React App
+          Learn Sample React App
         </a>
       </header>
     </div>
