23cddfa653c59b5c4d17e46add98cf2e14e650dc
Anh <voduyanh1984@gmail.com>
2020-06-16 10:01:30 +0700

    - make clean up

--- admin/src/index.js
+++ admin/src/index.js
@@ -10,7 +10,7 @@ import "./assets/css/pe-icon-7-stroke.css";
 import InventoryLayout from './layouts/inventory';
 
 // import './index.css';
-// import App from '././components/HelloWorld';
+// import App from '././components/App';
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(
