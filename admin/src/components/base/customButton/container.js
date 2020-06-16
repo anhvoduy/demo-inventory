4708de3026f6361b6da71d0e6ab1b0f5c561c2f6
Anh <voduyanh1984@yahoo.com>
2020-06-16 10:15:12 +0700

    FE make clean up (#25)
    * - make clean up
    
    * - clean up
    
    * - make updates
    
    * - move components
    
    * - clean code

--- admin/src/components/base/customButton/container.js
+++ admin/src/components/base/customButton/container.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
-import { Button } from "react-bootstrap";
-import cx from "classnames";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
 
 class CustomButton extends Component {
   render() {
     const { fill, simple, pullRight, round, block, ...rest } = this.props;
-
+    
     const btnClasses = cx({
-      "btn-fill": fill,
-      "btn-simple": simple,
-      "pull-right": pullRight,
-      "btn-block": block,
-      "btn-round": round
+      'btn-fill': fill,
+      'btn-simple': simple,
+      'pull-right': pullRight,
+      'btn-block': block,
+      'btn-round': round
     });
 
     return <Button className={btnClasses} {...rest} />;
