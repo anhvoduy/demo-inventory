d21e4aea49472c000db55f8286749ddf9df588e9
Anh <voduyanh1984@gmail.com>
2020-06-16 10:03:58 +0700

    - clean up

--- admin/src/components/App/container.js
+++ admin/src/components/App/container.js
@@ -1,10 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD:admin/src/components/HelloWorld/container.js
 import logo from '../../images/logo.svg';
-=======
-import info from '../package.json';
-import logo from './logo.svg';
->>>>>>> master:admin/src/App.js
 import './App.css';
 
 function App() {
@@ -19,14 +14,11 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-<<<<<<< HEAD:admin/src/components/HelloWorld/container.js
           rel="noopener noreferrer"
         >
           Learn React Sample App
-=======
           rel="noopener noreferrer">
           Inventory: { info.version }
->>>>>>> master:admin/src/App.js
         </a>
       </header>
     </div>
