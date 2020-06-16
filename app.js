8497d3f1aebddfccea851b729757c1c1aa201a5c
anhvoduy <voduyanh1984@gmail.com>
2020-06-17 00:25:06 +0700

    - tweak routing

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const path = require('path');
 const app = express();
 app.set('port', process.env.PORT || 8080);
 
-app.use(express.static(path.join(__dirname, 'pushcoins/build')));
+// const siteApp = path.join(__dirname, 'pushcoins/build');
+// app.use('/', express.static(siteApp, { index: 'index.html '}));
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'pushcoins/build', 'index.html'));
+// });
 
-// https://facebook.github.io/create-react-app/docs/deployment
-// Express example above to serve index.html for any unknown paths
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'pushcoins/build', 'index.html'));
+const siteAdmin = path.join(__dirname, 'admin/build');
+app.use('/', express.static(siteAdmin, { index: 'index.html '}));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'admin/build', 'index.html'));
 });
 
 app.listen(app.get('port'), function () {
   console.log('React Coins is running on port:' + app.get('port'));
-  //console.log('React App Inventory is running on port:' + app.get('port'));
 });
