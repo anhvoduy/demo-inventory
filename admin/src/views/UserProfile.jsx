8343e7b0c784f896b4cf8b13c1a2540bd8a117be
Anh <voduyanh1984@gmail.com>
2020-06-16 11:07:05 +0700

    - tweak base components

--- admin/src/views/UserProfile.jsx
+++ admin/src/views/UserProfile.jsx
@@ -1,20 +1,3 @@
-/*!
-
-=========================================================
-* Light Bootstrap Dashboard React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React, { Component } from "react";
 import {
   Grid,
@@ -25,7 +8,7 @@ import {
   FormControl
 } from "react-bootstrap";
 
-import { Card } from "components/Card/Card.jsx";
+import Card from '../components/base/card';
 import { FormInputs } from "components/FormInputs/FormInputs.jsx";
 import { UserCard } from "components/UserCard/UserCard.jsx";
 import Button from "components/CustomButton/CustomButton.jsx";
