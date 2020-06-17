951a0360a7c6d2fb511f06e3c0a4b4988a5e36d9
devinsg <duyanh2005@gmail.com>
2020-06-17 09:03:41 +0700

    - clean up

--- app.js
+++ app.js
@@ -2,13 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.set('port', process.env.PORT || 8080);
-
-// const siteApp = path.join(__dirname, 'pushcoins/build');
-// app.use('/', express.static(siteApp, { index: 'index.html '}));
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'pushcoins/build', 'index.html'));
-// });
+app.set('port', process.env.PORT || 3000);
 
 const siteAdmin = path.join(__dirname, 'admin/build');
 app.use('/', express.static(siteAdmin, { index: 'index.html '}));
@@ -17,5 +11,5 @@ app.get('/', function (req, res) {
 });
 
 app.listen(app.get('port'), function () {
-  console.log('React Coins is running on port:' + app.get('port'));
+  console.log('Inventory is running on port:' + app.get('port'));
 });
