f2ac4e1140302bc09207029cbfc4ef4ac9b9bbc8
Anh <voduyanh1984@gmail.com>
2020-06-25 17:27:04 +0700

    - updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
+set -e
 
-# this file for MACOS and LINUX
-# NODE_SOCKET_NS to define namespace for each environemt and each clients
+if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
+  set -- node "$@"
+fi
 
-#1, Build Logs and costing
-#npm i forever -g
-
-# 2.Start node
-cd ..
-# Update and Private dependence libs if have someone
-#nodemon server.js
-#forever -w --watchDirectory /usr/src/app start server.js
-#forever --fifo logs 0 to track realtime
-NODE_ENV=local NODE_SOCKET_NS=local /root/.nvm/versions/node/v12.15.0/bin/node server.js
+exec "$@"
