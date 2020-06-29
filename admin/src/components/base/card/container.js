bec0d69cb8f4c9235c990919405ae1d8690aaad9
shadow <9761264+devinsg@users.noreply.github.com>
2020-06-29 23:21:05 +0700

    - make clean css (#42)

--- admin/src/components/base/card/container.js
+++ admin/src/components/base/card/container.js
@@ -2,23 +2,20 @@ import React, { Component } from 'react';
 
 export class Card extends Component {
   render() {
+    const {ctAllIcons, ctTableFullWidth, ctTableResponsive, ctTableUpgrade} = this.props;
+    const clsContent = 'content' +
+      (ctAllIcons ? ' all-icons' : '') +
+      (ctTableFullWidth ? ' table-full-width' : '') +
+      (ctTableResponsive ? ' table-responsive' : '') +
+      (ctTableUpgrade ? ' table-upgrade' : '');
     return (
       <div className={'card' + (this.props.plain ? ' card-plain' : '')}>
         <div className={'header' + (this.props.hCenter ? ' text-center' : '')}>
           <h4 className='title'>{this.props.title}</h4>
           <p className='category'>{this.props.category}</p>
         </div>
-        <div
-          className={
-            'content' +
-            (this.props.ctAllIcons ? ' all-icons' : '') +
-            (this.props.ctTableFullWidth ? ' table-full-width' : '') +
-            (this.props.ctTableResponsive ? ' table-responsive' : '') +
-            (this.props.ctTableUpgrade ? ' table-upgrade' : '')
-          }
-        >
+        <div className={clsContent}>
           {this.props.content}
-
           <div className='footer'>
             {this.props.legend}
             {this.props.stats != null ? <hr /> : ''}
