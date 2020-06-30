8d68e839cc93c01884867c3934c71b4cb244d92a
Anh <voduyanh1984@gmail.com>
2020-06-30 12:21:17 +0700

    - refractor Footer

--- admin/src/components/base/sidebar/navContainer.js
+++ admin/src/components/base/sidebar/navContainer.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 const NavContainer = function({ key, upgrade, layout, path, icon, name, activeRoute }) {
-    const { t } = useTranslation();    
+    const { t } = useTranslation();
     return (
         <li className={ upgrade ? "active active-pro" : activeRoute(layout + path) } key={key}>
             <NavLink  to={layout + path} className="nav-link" activeClassName="active">
