13097093090e2c94f1ef744337b4e64ea5d7b738
Anh <voduyanh1984@gmail.com>
2020-06-30 16:09:06 +0700

    - disabled dropdown background color & clean up

--- admin/src/components/base/sidebar/navContainer.js
+++ admin/src/components/base/sidebar/navContainer.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
-const NavContainer = function({ key, upgrade, layout, path, icon, name, activeRoute }) {
+const NavContainer = function({ upgrade, layout, path, icon, name, activeRoute }) {
     const { t } = useTranslation();
     return (
-        <li className={ upgrade ? "active active-pro" : activeRoute(layout + path) } key={key}>
+        <li className={ upgrade ? "active active-pro" : activeRoute(layout + path) }>
             <NavLink  to={layout + path} className="nav-link" activeClassName="active">
                 <i className={icon} />
                 <p>{t(name)}</p>
