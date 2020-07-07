334fb47bd30aae060d325a35cf451434cf0b232d
Anh <voduyanh1984@yahoo.com>
2020-07-08 00:46:24 +0700

    - test build (#55)

--- admin/src/views/reports/rpt002/container.js
+++ admin/src/views/reports/rpt002/container.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import { Grid, Row, Col } from 'react-bootstrap';
+import Card from '../../../components/base/card';
 
 const Rpt002 = function() {  
   return (
