96eb161264ac26a1c5aec6e284fde1219f35dfb1
devinsg <duyanh2005@gmail.com>
2020-07-22 14:28:13 +0700

    - clean code

--- admin/src/views/forrest/container.js
+++ admin/src/views/forrest/container.js
@@ -4,19 +4,7 @@ import { Grid, Row, Col } from 'react-bootstrap';
 
 import Card from '../../components/base/card';
 import StatsCard from '../../components/base/statsCard';
-import Tasks from '../../components/base/tasks';
-import {
-  dataPie,
-  legendPie,
-  dataSales,
-  optionsSales,
-  responsiveSales,
-  legendSales,
-  dataBar,
-  optionsBar,
-  responsiveBar,
-  legendBar
-} from 'variables';
+import { dataSales, optionsSales, responsiveSales, legendSales } from 'variables';
 
 const createLegend = function(json) {
   var legend = [];
@@ -36,15 +24,6 @@ class Container extends Component {
           <Row>
             <Col lg={3} sm={6}>
               <StatsCard
-                bigIcon={<i className='pe-7s-server text-warning' />}
-                statsText='Capacity'
-                statsValue='105GB'
-                statsIcon={<i className='fa fa-refresh' />}
-                statsIconText='Updated now'
-              />
-            </Col>
-            <Col lg={3} sm={6}>
-              <StatsCard
                 bigIcon={<i className='pe-7s-wallet text-success' />}
                 statsText='Revenue'
                 statsValue='$1,345'
@@ -58,21 +37,12 @@ class Container extends Component {
                 statsText='Errors'
                 statsValue='23'
                 statsIcon={<i className='fa fa-clock-o' />}
-                statsIconText='In the last hour'
-              />
-            </Col>
-            <Col lg={3} sm={6}>
-              <StatsCard
-                bigIcon={<i className='fa fa-twitter text-info' />}
-                statsText='Followers'
-                statsValue='+45'
-                statsIcon={<i className='fa fa-refresh' />}
-                statsIconText='Updated now'
+                statsIconText='Last hour'
               />
             </Col>
           </Row>
           <Row>
-            <Col md={8}>
+            <Col md={12}>
               <Card
                 statsIcon='fa fa-history'
                 id='chartHours'
@@ -89,69 +59,7 @@ class Container extends Component {
                     />
                   </div>
                 }
-                legend={
-                  <div className='legend'>{createLegend(legendSales)}</div>
-                }
-              />
-            </Col>
-            <Col md={4}>
-              <Card
-                statsIcon='fa fa-clock-o'
-                title='Email Statistics'
-                category='Last Campaign Performance'
-                stats='Campaign sent 2 days ago'
-                content={
-                  <div
-                    id='chartPreferences'
-                    className='ct-chart ct-perfect-fourth'
-                  >
-                    <ChartistGraph data={dataPie} type='Pie' />
-                  </div>
-                }
-                legend={
-                  <div className='legend'>{createLegend(legendPie)}</div>
-                }
-              />
-            </Col>
-          </Row>
-
-          <Row>
-            <Col md={6}>
-              <Card
-                id='chartActivity'
-                title='2014 Sales'
-                category='All products including Taxes'
-                stats='Data information certified'
-                statsIcon='fa fa-check'
-                content={
-                  <div className='ct-chart'>
-                    <ChartistGraph
-                      data={dataBar}
-                      type='Bar'
-                      options={optionsBar}
-                      responsiveOptions={responsiveBar}
-                    />
-                  </div>
-                }
-                legend={
-                  <div className='legend'>{createLegend(legendBar)}</div>
-                }
-              />
-            </Col>
-
-            <Col md={6}>
-              <Card
-                title='Tasks'
-                category='Backend development'
-                stats='Updated 3 minutes ago'
-                statsIcon='fa fa-history'
-                content={
-                  <div className='table-full-width'>
-                    <table className='table'>
-                      <Tasks />
-                    </table>
-                  </div>
-                }
+                legend={ <div className='legend'>{createLegend(legendSales)}</div> }
               />
             </Col>
           </Row>
