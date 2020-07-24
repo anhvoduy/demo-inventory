36e49137fa9be20e133e47aae5cbe3d8f86f3ce0
Anh <voduyanh1984@gmail.com>
2020-07-24 11:46:34 +0700

    - setup table history

--- admin/src/views/forrest/lineTable/container.js
+++ admin/src/views/forrest/lineTable/container.js
@@ -1,41 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Table } from 'react-bootstrap';
 import Card from '../../../components/base/card';
 import { thArray, tdArray } from '../../../variables';
 
-class Container extends Component {
-  render() {
-    return (
-      <Card statsIcon='fa fa-history'
-            id='chartHours'
-            title='Users Behavior'
-            category='24 Hours performance'
-            stats='Updated 3 minutes ago'
-            content={
-              <Table striped hover>
-                <thead>
-                  <tr>
-                    {thArray.map((prop, key) => {
-                      return <th key={key}>{prop}</th>;
-                    })}
-                  </tr>
-                </thead>
-                <tbody>
-                  {tdArray.map((prop, key) => {
+const LineTable = () => {  
+  return (
+    <Card statsIcon='fa fa-history'
+          id='goldTable'
+          title='Gold Prices'
+          category='Today'
+          stats='Updated 3 minutes ago'
+          content={
+            <Table striped hover>
+              <thead>
+                <tr>
+                  {
+                    thArray.map((prop, key) => { return <th key={key}>{prop}</th>; })
+                  }
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  tdArray.map((prop, key) => {
                     return (
                       <tr key={key}>
-                        {prop.map((prop, key) => {
-                          return <td key={key}>{prop}</td>;
-                        })}
+                        {
+                          prop.map((prop, key) => { return <td key={key}>{prop}</td>; })
+                        }
                       </tr>
                     );
-                  })}
-                </tbody>
-              </Table>
-            }
-      />
-    );
-  }
+                  })
+                }
+              </tbody>
+            </Table>
+          }
+    />
+  );  
 }
 
-export default Container;
+export default LineTable;
